23f14fc9d456814b8a3b9f6aea85dae47b6467ec
Shakil Thakur <thaku019@umn.edu>
2014-05-18 00:02:45 -0500

    boidmodule is now using the new vector class and middleclass

--- boidmodule.lua
+++ boidmodule.lua
@@ -7,28 +7,9 @@
 -- @script Boid
 
 -- Imports
+local class       = require 'libs/middleclass/middleclass'
 local PathFinding = require 'pathfinding'
-
--- ================
--- Private helpers
--- ================
-
-local setmetatable = setmetatable
-local tostring     = tostring
-local unpack       = unpack
-
--- Internal class constructor
-local class = function(...)
-local klass = {}
-klass.__index = klass
-klass.__call = function(_,...) return klass:new(...) end
-function klass:new(...)
-    local instance = setmetatable({}, klass)
-    klass.__init(instance, ...)
-    return instance
-end
-return setmetatable(klass,{__call = klass.__call})
-end
+local Vector      = require 'vector'
 
 function dotProduct(x1, y1, x2, y2)
     return x1 * x2 + y1 * y2
@@ -51,92 +32,6 @@ local colors = {
 -- ================
 -- Module classes
 -- ================
-
---- `Vector` class
--- @type Vector
-local Vector = class()
-Vector.__eq = function(a, b) return (a.p1 == b.p1 and a.p2 == b.p2) end
-Vector.__add = function(a, b) return Vector:new(a.x + b.x, a.y + b.y) end
-Vector.__mult = function(a, b) return Vector:new(a.x * b.x, a.y * b.y) end
-Vector.__sub = function(a, b) return Vector:new(a.x - b.x, a.y - b.y) end
-Vector.__div = function(a, b) return Vector:new(a.x / b.x, a.y / b.y) end
-Vector.__tostring = function(v)
-return (('Vector :  %f  %f'):format(tostring(v.x), tostring(v.y)))
-end
-
-
-
---- Creates a new `Vector`
--- @name Vector:new
--- @param x a float
--- @param y a float
--- @return a new `Vector`
--- @usage
--- local Vector = require 'Vector'
--- local v = Vector:new(x,y,width,height)
--- local v = Vector(x,y) -- Alias to Vector:new
--- print(v) -- print the Vector properties
---
-function Vector:__init(x, y)
-    self.x, self.y = x, y 
-end
-
-function Vector:rotate(angle)
-    local x = (self.x*math.cos(angle)) - (self.y*math.sin(angle))
-    local y = (self.x*math.sin(angle)) + (self.y*math.cos(angle))
-    return Vector:new(x, y)
-end
-
-function Vector:length()
-    return math.sqrt(self.x^2 + self.y^2);
-end
-
-function Vector:unit()
-    local length = self:length()
-    return Vector:new(self.x/length, self.y/length)
-end
-
-function Vector:dot(a)
-    return self.x * a.x + self.y * a.y
-end
-
-function Vector:normalize()
-    local mag = self:length();
-    return Vector:new(self.x/mag, self.y/mag)
-end
-
-function Vector:scalarMult(scalar)
-    return Vector:new(self.x*scalar,self.y*scalar)
-end
-
-function Vector:scalarSub(scalar)
-    return Vector:new(self.x-scalar,self.y-scalar)
-end
-
-function Vector:scalarDiv(scalar)
-    return Vector:new(self.x/scalar,self.y/scalar)
-end
-
-function Vector:upperLimit(limit)
-    local x = self.x
-    local y = self.y
-    if x > limit then
-        x = limit
-    end
-    if y > limit then 
-        y = limit
-    end
-    return Vector:new(x,y)
-end
-
-function Vector:projection(b)
-    -- local bUnit = b:unit()
-    -- local scalar = self:dot(b) / b:length()
-    -- return bUnit:scalarMult(scalar), self:dot(b)
-    local dot = self:dot(b)
-    return b:scalarMult(dot), dot
-end
-
 -- handles changes to paths
 local colorToPath = {}
 -- keeps track of the amount of boids in each path
@@ -149,11 +44,11 @@ local algoToColor = {
 }
 --- `Boid` class
 -- @type Boid
-local Boid = class()
+local Boid = class("Boid")
 Boid.__eq = function(a, b) return (a.id == b.id) end
 Boid.__tostring = function(r) return "" end
 
-function Boid:__init(id, x, y, path, color)
+function Boid:initialize(id, x, y, path, color)
     self.id = id
 
     -- Position Velocity Acceleration
@@ -241,12 +136,12 @@ end
 
 --- `Motorcade` class
 -- @type Motorcade
-local Motorcade = class()
+local Motorcade = class("Motorcade")
 
 Motorcade.__eq = function(a, b) return false end
 Motorcade.__tostring = function(r) return "" end
 
-function Motorcade:__init(roadRadius)
+function Motorcade:initialize(roadRadius)
     self.boids = {}
     self.roadRadius = roadRadius
     self.numAStar = 0
@@ -433,13 +328,9 @@ function Motorcade:remove(number, color)
     end 
 end
 
-
-
-
 BoidModule = {
     Boid = Boid,
     Motorcade = Motorcade,
-    Vector = Vector,
     _VERSION = "SUPER-BETA"
 }
 return BoidModule
